44ceefa5036182b36ed6079aeebe9d74249b6bba
Kenneth <sally@sallyde-MacBook-Air.local>
2019-10-26 16:22:51 -0700

    add layout to homepage, able to render the picture for hairstylists

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import { Layout, Menu, Card, Col, Row } from 'antd';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+const { Header, Content, Footer } = Layout;
+const { Meta } = Card;
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+export default function App () {
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+  
+  // const fetchData = () => {
+  //   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+  //   .then((response) => {
+  //     // handle success
+  //     console.log(response.data) // The entire response from the Rails API
 
-  render() {
+  //     console.log(response.data.message) // Just the message
+  //     setMessage({
+  //       message: response.data.message
+  //     });
+  //   }) 
+  // }
+
+  
     return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
+      <Layout className="layout">
+
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">Hairstylist</Menu.Item>
+          <Menu.Item key="3">Products</Menu.Item>
+          <Menu.Item key="4">Services</Menu.Item>
+          <Menu.Item key="5">About Us</Menu.Item>
+          <Menu.Item key="6">Login</Menu.Item>
+          <Menu.Item key="7">Register</Menu.Item>
+
+        </Menu>
+      </Header>
+
+      <Content style={{ padding: '0 50px' }}>
+
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          <Row gutter={16}>
+            
+            <Col span={8}>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              >
+                <Meta title="Johnny J" description="Lead Hairstylist" />
+              </Card>
+            </Col>
+
+            <Col span={8}>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              >
+                <Meta title="Johnny J" description="Lead Hairstylist" />
+              </Card>
+            </Col>
+
+           </Row> 
+        </div>
+
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED
+
+      </Footer>
+
+        </Layout>
     );
-  }
+
 }
 
-export default App;
